e587b04b57e98cbcb1a803889025616682cb2a1a
Samuel Ogu <hello@samuelogu.com>
2021-03-22 13:34:11 +0100

    updated auth validator

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -4,12 +4,12 @@ const schemas = {
     register: Joi.object().keys({
         firstName: Joi.string().required(),
         lastName: Joi.string().required(),
-        email: Joi.string().email().required(),
+        email: Joi.string().email().lowercase().required(),
         password: Joi.string().min(6).required()
     }),
     login: Joi.object().keys({
         email: Joi.string().email().required(),
-        password: Joi.string().required()
+        password: Joi.string().min(6).required()
     })
 }
 module.exports = schemas;
