66eb430a732a451a2b7370f33a0d68e46355fd07
Samuel Ogu <hello@samuelogu.com>
2021-05-07 06:09:31 +0100

    updates

--- src/app.js
+++ src/app.js
@@ -3,11 +3,9 @@ const app = express();
 
 // connect to mongodb and redis
 require('./connectors/redis')
-require('./connectors/mongodb')
 
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
-const mongoose = require("mongoose")
 const multer = require('multer')
 const cors = require('cors')
 
